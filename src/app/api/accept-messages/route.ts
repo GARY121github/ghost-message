a842564fd7d5213371df2baee0cfbd2433846d94
gary <harshprakash000@gmail.com>
2024-04-20 18:47:56 +0530

    Add API routes for sending and getting messages

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,125 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/user.model";
+import { User } from "next-auth";
+
+export async function POST(request: Request) {
+    await dbConnect();
+
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user;
+
+    if (!session || !session.user) {
+        return Response.json(
+            {
+                success: false,
+                message: "Not Authencaited"
+            },
+            {
+                status: 401
+            }
+        )
+    }
+
+    const userId = user.id;
+    const { acceptMessage } = await request.json();
+
+    try {
+        const user = await UserModel.findByIdAndUpdate(
+            userId ,
+            { acceptMessage },
+            { new: true }
+        );
+
+        if(!user){
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found"
+                },
+                {
+                    status: 404
+                }
+            )
+        };
+
+        return Response.json(
+            {
+                success: true,
+                message: "User accept message status changed successfully"
+            }
+        )
+    } catch (error) {
+        console.log("Error accepting messages", error);
+        return Response.json(
+            {
+                success: false,
+                message: "Failed to change user status to accept messages " + error
+            },
+            {
+                status: 500
+            }
+        )
+    }
+}
+
+
+export async function GET(request: Request) {
+    await dbConnect();
+
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user;
+
+    if (!session || !session.user) {
+        return Response.json(
+            {
+                success: false,
+                message: "Not Authencaited"
+            },
+            {
+                status: 401
+            }
+        )
+    }
+
+    const userId = user.id;
+
+    try {
+        const user = await UserModel.findById(userId);
+
+        if(!user){
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found"
+                },
+                {
+                    status: 404
+                }
+            )
+        }
+
+        return Response.json( 
+            {
+                success: true,
+                acceptMessage: user.isAcceptingMessage
+            },
+            {
+                status: 200
+            }
+        )
+        
+    } catch (error) {
+        console.log("Error accepting messages", error);
+        return Response.json(
+            {
+                success: false,
+                message: "Failed to get user status to accept messages " + error
+            },
+            {
+                status: 500
+            }
+        )
+    }
+}
---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,63 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/user.model";
+import { Message } from "@/models/user.model";
+
+export async function POST(request: Request) {
+    await dbConnect();
+    const {username , content } = await request.json();
+    try {
+        const user = await UserModel.findOne({username});
+        if(!user){
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found"
+                },
+                {
+                    status: 404
+                }
+            );
+        }
+
+        // check if user is accepting messages
+        if(!user.isAcceptingMessage){
+            return Response.json(
+                {
+                    success: false,
+                    message: "User is not accepting messages"
+                },
+                {
+                    status: 403
+                }
+            );
+        }
+
+        const message = {
+            content,
+            createdAt: new Date()
+        };
+
+        user.messages.push(message as Message);
+        await user.save();
+        return Response.json(
+            {
+                success: true,
+                message: "Message sent successfully"
+            },
+            {
+                status: 200
+            }
+        );
+    } catch (error) {
+        console.log("Error in sending message: ", error);
+        return Response.json(
+            {
+                success: false,
+                message: "Failed to send message " + error
+            },
+            {
+                status: 500
+            }
+        );
+    }
+}
