59bcccbff5ba6608f2c93ba0aa0424161cceb3c6
gary <harshprakash000@gmail.com>
2024-06-07 23:29:51 +0530

    fixed the small bug

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -422,7 +422,7 @@ export default function GlobeDemo() {
         </motion.div>
         <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-white to-black z-40" />
         <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
-          <World data={sampleArcs} globeConfig={globeConfig} />;
+          <World data={sampleArcs} globeConfig={globeConfig} />
         </div>
       </div>
     </div>
